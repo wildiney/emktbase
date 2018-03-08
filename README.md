5a5f246af4ffacc3d7b8a5bf700ee1505e350ac7
Fernando <wildiney@gmail.com>
2018-03-08 11:28:53 -0300

    Atualização para Gulp 4

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # emktbase
 Arquivos base para a construção de email marketing
+
+# ATUALIZAÇÃO 2.0.0
+Atualização para a utilização com GULP 4
--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,56 @@
 'use strict';
 
-var gulp            = require('gulp');
-var inlineCss       = require('gulp-inline-css');
-var strip           = require('gulp-strip-comments');
-var htmlmin         = require('gulp-htmlmin');
-var imagemin        = require('gulp-imagemin');
-var browserSync     = require('browser-sync').create();
-var os              = require('os');
-var open            = require('gulp-open');
-var browser         = os.platform() === 'linux' ? 'google-chrome' : (os.platform() === 'darwin' ? 'google chrome' : ( os.platform() === 'win32' ? 'chrome' : 'firefox'));
+var gulp = require('gulp');
+var strip = require('gulp-strip-comments');
+var inlineCSS = require('gulp-inline-css');
+var htmlmin = require('gulp-htmlmin');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+var os = require('os');
+var open = require('gulp-open');
+var browser = os.platform() === 'linux' ? 'google-chrome' : (os.platform() === 'darwin' ? 'google chrome' : (os.platform() === 'win32' ? 'chrome' : 'firefox'));
 
-gulp.task('html',['imagemin'], function () {
+function exec_inlineCSS_minify() {
     return gulp.src('./src/*.html')
         .pipe(strip())
-        .pipe(inlineCss())
-        .pipe(htmlmin({collapseWhitespace:true}))
+        .pipe(inlineCSS())
+        .pipe(htmlmin({ collapseWhitespace: true }))
         .pipe(gulp.dest('./dist/'))
         .pipe(browserSync.reload({
             stream: true
         }));
-});
-
-gulp.task('open', function(){
-    var options = {
-        uri:'localhost:3000',
-        app: 'firefox'
-    };
-    gulp.src(__filename)
-    .pipe(open(options));
-})
-
-gulp.task('imagemin',function(){
+}
+function exec_imagemin() {
     return gulp.src('./src/img/*')
         .pipe(imagemin())
         .pipe(gulp.dest('./dist/img/'))
         .pipe(browserSync.reload({
             stream: true
         }));
-});
-
-gulp.task('browserSync', ['html'], function () {
+}
+function exec_browserSync(done) {
     browserSync.init({
         server: {
             baseDir: "./dist"
         }
     });
-});
+    done();
+}
+function exec_open() {
+    var options = {
+        uri: 'localhost:3000',
+        app: 'firefox'
+    };
+    gulp.src(__filename)
+        .pipe(open(options));
+}
+gulp.task('html', exec_inlineCSS_minify);
+gulp.task('imagemin', exec_imagemin);
+gulp.task('browserSync', exec_browserSync);
+gulp.task('open', exec_open);
 
-gulp.task('watch', ['browserSync'], function(){
-    gulp.watch("./src/*.html",['html']);
-    gulp.watch("./src/img/*",['imagemin']);
-});
 
-gulp.task('default', ['html', 'browserSync','imagemin','open','watch']);
+var watcher = gulp.watch('./src/**/*', gulp.series('html', 'imagemin'));
+watcher.on('change', browserSync.reload)
+
+gulp.task('default', gulp.series('html', 'browserSync', 'open'));
